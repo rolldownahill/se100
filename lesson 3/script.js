6fd57d24bbbf9874870458bfe2f29d9f062afbbc
Magdelene Phua <magdelene_phua@outlook.com>
2025-02-17 22:44:02 +0800

    x

---
+++ lesson 3/script.js
@@ -0,0 +1,13 @@
+let stockprice = 300
+console.log(stockprice)
+
+stockprice += 500
+console.log(stockprice)
+
+// Complete the code below
+
+let company = 'Microsofts'
+console.log(company);
+
+company = 'Microsoft\'s'
+console.log(company);
