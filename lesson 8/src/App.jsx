5b22612361d9b27ddff3a555844b42f4b43fca75
Magdelene Phua <magdelene_phua@outlook.com>
2025-03-30 15:39:10 +0800

    x

---
+++ lesson 8/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Currency Converter</h1>
+      <button>
+        Click me
+      </button>
+    </>
+  )
+}
+
+export default App;
